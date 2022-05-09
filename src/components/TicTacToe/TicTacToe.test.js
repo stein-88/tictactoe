985ac6117380021bb73679ad7e0ef28fdb0b0b75
stein-88 <wanderson.code@icloud.com>
2022-05-09 16:16:10 -0300

    fix unit test

--- src/components/TicTacToe/TicTacToe.test.js
+++ src/components/TicTacToe/TicTacToe.test.js
@@ -1,52 +1,24 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import TicTacToe from './TicTacToe'
-import StartGame from './StartGame'
 import { Provider } from 'react-redux'
 import configureStore from '../../store'
 import schema from './StartGame/data'
-const { players, titulo } = schema
-const verify = [titulo]
+const { players } = schema
+jest.mock('uuid', () => ({ v4: () => `uniquekey${Math.random(1000) * 100}` }))
 
 describe('Test on TicTacToe Component', () => {
-    test('Should render without props and start on StartGame page', () => {
-        const compTicTacToe = renderer.create(
-            <Provider store={configureStore()}>
-                <TicTacToe />
-            </Provider>
-        )
-        const jsonTicTacToe = compTicTacToe.toJSON()
-        const Result = compTicTacToe.root.findAllByProps({
-            ['data-test']: 'players'
-        })
-        expect(Result).toHaveLength(players.length)
-        expect(jsonTicTacToe).toMatchSnapshot()
-    })
-})
-describe('Test on StartGame Component', () => {
-	const compStartGame = renderer.create(<StartGame changeToPage={() => null} />)
-
-	test('Should not render without props', () => {
-		const compStartGame = renderer.create(<StartGame />)
-		const jsonStartGame = compStartGame.toJSON()
-		expect(jsonStartGame).toBeNull()
-	})
-	test('Should render with props and have 2players only', () => {
-		const jsonStartGame = compStartGame.toJSON()
-		const Result = compStartGame.root.findAllByProps({
+	test('Should render without props and start on StartGame page', () => {
+		const compTicTacToe = renderer.create(
+			<Provider store={configureStore()}>
+				<TicTacToe />
+			</Provider>
+		)
+		const jsonTicTacToe = compTicTacToe.toJSON()
+		const Result = compTicTacToe.root.findAllByProps({
 			['data-test']: 'players'
 		})
-		expect(jsonStartGame).toMatchSnapshot()
 		expect(Result).toHaveLength(players.length)
-	})
-	test('The component should have just 1 title h1', () => {
-		const Result = compStartGame.root.findAllByProps({ className: 'h1 pb-5' })
-		expect(Result).toHaveLength(verify.length)
-	})
-	test('Should have 1 buttom with aria-hidden', () => {
-		const Result = compStartGame.root.findAllByProps({ className: 'btnstyle' })
-		const checkAria = compStartGame.root.findAllByProps({ ['aria-hidden']: "true" })
-		expect(Result).toHaveLength(1)
-		expect(checkAria).toHaveLength(1)
+		expect(jsonTicTacToe).toMatchSnapshot()
 	})
 })
--- src/container/App.test.js
+++ src/container/App.test.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import App from './App'
-import TicTacToe from '../components/TicTacToe'
-import StartGame from '../components/TicTacToe/StartGame'
-import schema from '../components/TicTacToe/StartGame/data'
 import { Provider } from 'react-redux'
 import configureStore from '../store'
 
-const { players, titulo } = schema
-const verify = [titulo]
+jest.mock('uuid', () => ({ v4: () => `uniquekey${Math.random(1000) * 100}` }))
 
 describe('Test on App container', () => {
 	test('Renderized with no props', () => {
@@ -19,47 +15,5 @@ describe('Test on App container', () => {
 		)
 		const compJson = compNoProps.toJSON()
 		expect(compJson).toMatchSnapshot()
-	})
-})
-describe('Test on TicTacToe Component', () => {
-	test('Should render without props and start on StartGame page', () => {
-		const compTicTacToe = renderer.create(
-			<Provider store={configureStore()}>
-				<TicTacToe />
-			</Provider>
-		)
-		const jsonTicTacToe = compTicTacToe.toJSON()
-		const Result = compTicTacToe.root.findAllByProps({
-			['data-test']: 'players'
-		})
-		expect(Result).toHaveLength(players.length)
-		expect(jsonTicTacToe).toMatchSnapshot()
-	})
-})
-describe('Test on StartGame Component', () => {
-	const compStartGame = renderer.create(<StartGame changeToPage={() => null} />)
-
-	test('Should not render without props', () => {
-		const compStartGame = renderer.create(<StartGame />)
-		const jsonStartGame = compStartGame.toJSON()
-		expect(jsonStartGame).toBeNull()
-	})
-	test('Should render with props and have 2players only', () => {
-		const jsonStartGame = compStartGame.toJSON()
-		const Result = compStartGame.root.findAllByProps({
-			['data-test']: 'players'
-		})
-		expect(jsonStartGame).toMatchSnapshot()
-		expect(Result).toHaveLength(players.length)
-	})
-	test('The component should have just 1 title h1', () => {
-		const Result = compStartGame.root.findAllByProps({ className: 'h1 pb-5' })
-		expect(Result).toHaveLength(verify.length)
-	})
-	test('Should have 1 buttom with aria-hidden', () => {
-		const Result = compStartGame.root.findAllByProps({ className: 'btnstyle' })
-		const checkAria = compStartGame.root.findAllByProps({ ['aria-hidden']: "true" })
-		expect(Result).toHaveLength(1)
-		expect(checkAria).toHaveLength(1)
 	})
 })
